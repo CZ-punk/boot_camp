3ce81d3472ecce0cdadead2675172c811d7034e6
CZ-punk <cnhhw0408@gmail.com>
2024-07-19 16:28:28 +0900

    지연로딩 + 영속성 컨텍스트

--- 숙련주차/숙련week_2/jpa-advance/src/main/java/com/sparta/jpaadvance/repository/OrderRepository.java
+++ 숙련주차/숙련week_2/jpa-advance/src/main/java/com/sparta/jpaadvance/repository/OrderRepository.java
@@ -1,7 +1,7 @@
-package com.sparta.jpaadvance.repository;
-
-import com.sparta.jpaadvance.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
+//package com.sparta.jpaadvance.repository;
+//
+//import com.sparta.jpaadvance.entity.Order;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface OrderRepository extends JpaRepository<Order, Long> {
+//}
