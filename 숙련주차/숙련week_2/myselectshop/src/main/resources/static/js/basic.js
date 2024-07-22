db160dc2c074b2cfd7a3657e1d3f34082113a31b
CZ-punk <cnhhw0408@gmail.com>
2024-07-22 11:58:09 +0900

    카카오 사용자 정보 및 엑세스 토큰 가져오기

--- 숙련주차/숙련week_2/myselectshop/src/main/resources/static/js/basic.js
+++ 숙련주차/숙련week_2/myselectshop/src/main/resources/static/js/basic.js
@@ -423,15 +423,21 @@ function setMyprice() {
 
 function logout() {
     // 토큰 삭제
-    Cookies.remove('Authorization', {path: '/'});
+    Cookies.remove('Auth    orization', {path: '/'});
     window.location.href = host + '/api/user/login-page';
 }
 
 function getToken() {
+
     let auth = Cookies.get('Authorization');
 
     if(auth === undefined) {
         return '';
+    }
+
+    // kakao 로그인 사용한 경우 Bearer 추가
+    if(auth.indexOf('Bearer') === -1 && auth !== ''){
+        auth = 'Bearer ' + auth;
     }
 
     return auth;
